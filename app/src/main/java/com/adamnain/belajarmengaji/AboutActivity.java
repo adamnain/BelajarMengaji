2b8cecbd1bf098b6cbe8dbf90b989b687a3eba65
adamnain <adamdzulqarnain@gmail.com>
2018-12-25 18:17:14 +0700

    add abou for praktikum

--- app/src/main/java/com/adamnain/belajarmengaji/AboutActivity.java
+++ app/src/main/java/com/adamnain/belajarmengaji/AboutActivity.java
@@ -8,6 +8,6 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_about_praktikum);
     }
 }
