f8f1b9e6a23cb28989b48076702170193de02ffb
adamnain <adamdzulqarnain@gmail.com>
2018-12-27 11:15:53 +0700

    add all algorithm

---
+++ app/src/main/java/com/adamnain/belajarmengaji/activity/MainActivity.java
@@ -0,0 +1,71 @@
+package com.adamnain.belajarmengaji.activity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
+import com.adamnain.belajarmengaji.R;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton pindah;
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final MediaPlayer suaraButton = MediaPlayer.create(this, R.raw.button);
+        mp = MediaPlayer.create(getBaseContext(),R.raw.backsound);
+        mp.start();
+
+        pindah = (ImageButton) findViewById(R.id.buttonBelajar);
+        pindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                suaraButton.start();
+                Intent intent = new Intent(MainActivity.this,BelajarActivity.class);
+                startActivity(intent);
+                mp.stop();
+            }
+        });
+
+        pindah = (ImageButton) findViewById(R.id.buttonKuis);
+        pindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                suaraButton.start();
+                Intent intent = new Intent(MainActivity.this,KuisActivity.class);
+                startActivity(intent);
+                mp.stop();
+            }
+        });
+
+        pindah = (ImageButton) findViewById(R.id.buttonAbout);
+        pindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                suaraButton.start();
+                Intent intent = new Intent(MainActivity.this,AboutActivity.class);
+                startActivity(intent);
+                mp.stop();
+            }
+        });
+
+        pindah = (ImageButton) findViewById(R.id.buttonExit);
+        pindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                suaraButton.start();
+                finish();
+                mp.stop();
+            }
+        });
+
+    }
+}
